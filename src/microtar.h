b49a75bbf6e065a02be43fceea44d7eb82a2df2e
BCadet <19406430+BCadet@users.noreply.github.com>
2023-05-24 11:16:51 +0200

    update LICENSE + codding style

--- src/microtar.h
+++ src/microtar.h
@@ -1,5 +1,6 @@
 /**
  * Copyright (c) 2017 rxi
+ * Copyright (c) 2023 BCadet
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the MIT license. See `microtar.c` for details.
@@ -18,54 +19,56 @@ extern "C"
 
 #define MTAR_VERSION "0.1.0"
 
-enum {
-  MTAR_ESUCCESS     =  0,
-  MTAR_EFAILURE     = -1,
-  MTAR_EOPENFAIL    = -2,
-  MTAR_EREADFAIL    = -3,
-  MTAR_EWRITEFAIL   = -4,
-  MTAR_ESEEKFAIL    = -5,
-  MTAR_EBADCHKSUM   = -6,
-  MTAR_ENULLRECORD  = -7,
-  MTAR_ENOTFOUND    = -8
+enum
+{
+    MTAR_ESUCCESS = 0,
+    MTAR_EFAILURE = -1,
+    MTAR_EOPENFAIL = -2,
+    MTAR_EREADFAIL = -3,
+    MTAR_EWRITEFAIL = -4,
+    MTAR_ESEEKFAIL = -5,
+    MTAR_EBADCHKSUM = -6,
+    MTAR_ENULLRECORD = -7,
+    MTAR_ENOTFOUND = -8
 };
 
-enum {
-  MTAR_TREG   = '0',
-  MTAR_TLNK   = '1',
-  MTAR_TSYM   = '2',
-  MTAR_TCHR   = '3',
-  MTAR_TBLK   = '4',
-  MTAR_TDIR   = '5',
-  MTAR_TFIFO  = '6'
+enum
+{
+    MTAR_TREG = '0',
+    MTAR_TLNK = '1',
+    MTAR_TSYM = '2',
+    MTAR_TCHR = '3',
+    MTAR_TBLK = '4',
+    MTAR_TDIR = '5',
+    MTAR_TFIFO = '6'
 };
 
-typedef struct {
-  unsigned mode;
-  unsigned owner;
-  unsigned size;
-  unsigned mtime;
-  unsigned type;
-  char name[100];
-  char linkname[100];
+typedef struct
+{
+    unsigned mode;
+    unsigned owner;
+    unsigned size;
+    unsigned mtime;
+    unsigned type;
+    char name[100];
+    char linkname[100];
 } mtar_header_t;
-
 
 typedef struct mtar_t mtar_t;
 
-struct mtar_t {
-  int (*read)(mtar_t *tar, void *data, unsigned size);
-  int (*write)(mtar_t *tar, const void *data, unsigned size);
-  int (*seek)(mtar_t *tar, unsigned pos);
-  int (*close)(mtar_t *tar);
-  void *stream;
-  unsigned pos;
-  unsigned remaining_data;
-  unsigned last_header;
+struct mtar_t
+{
+    int (*read)(mtar_t *tar, void *data, unsigned size);
+    int (*write)(mtar_t *tar, const void *data, unsigned size);
+    int (*seek)(mtar_t *tar, unsigned pos);
+    int (*close)(mtar_t *tar);
+    void *stream;
+    unsigned pos;
+    unsigned remaining_data;
+    unsigned last_header;
 };
 
-
-const char* mtar_strerror(int err);
+const char *mtar_strerror(int err);
 
 int mtar_open(mtar_t *tar, const char *filename, const char *mode);
 int mtar_close(mtar_t *tar);
